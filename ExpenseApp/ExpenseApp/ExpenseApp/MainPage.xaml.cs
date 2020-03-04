e0f71a30dec32ae8a32c85a2b0a3b84dbce1b997
sky0cean <sky0cean@hotmail.com>
2020-03-03 17:20:34 -0800

    added expense part

--- ExpenseApp/ExpenseApp/ExpenseApp/MainPage.xaml.cs
+++ ExpenseApp/ExpenseApp/ExpenseApp/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,19 +14,52 @@ namespace ExpenseApp
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+
+        string filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"budget.txt");
+        string expenseFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "expense.txt");
+
         public MainPage()
         {
             InitializeComponent();
+            if (File.Exists(filename))
+            {
+                editor.Text= File.ReadAllText(filename);
+            }
+
+            if (File.Exists(expenseFileName))
+            {
+                editor.Text = File.ReadAllText(expenseFileName);
+            }
         }
 
         private void OnSaveButtonClicked(object sender, EventArgs e)
         {
-
+            File.WriteAllText(filename, editor.Text);
         }
 
         private void OnDeleteButtonClicked(object sender, EventArgs e)
         {
-
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+            editor.Text = string.Empty;
         }
+
+        private void OnExpenseSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(expenseFileName, expenseEditor.Text);
+        }
+
+        private void OnExpenseDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(expenseFileName))
+            {
+                File.Delete(expenseFileName);
+            }
+            expenseEditor.Text = string.Empty;
+        }
+
+
     }
 }
