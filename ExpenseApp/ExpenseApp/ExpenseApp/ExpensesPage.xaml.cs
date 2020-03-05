8559197d61b1c979247f358e3f68d555dabc255c
sky0cean <sky0cean@hotmail.com>
2020-03-04 19:39:02 -0800

    Added Expense page

---
+++ ExpenseApp/ExpenseApp/ExpenseApp/ExpensesPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpenseApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExpensesPage : ContentPage
+    {
+        public ExpensesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
